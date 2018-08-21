4188662adcc5387f243107a5764a2737fcb85e26
Unknown <rbeckstead_1991@hotmail.com>
2018-08-21 16:20:22 -0600

    start

---
+++ src/compents/Page2.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class Page2 extends Component {
+    render() {
+        return (
+            <div className="Page2">
+
+            </div>
+        );
+    }
+}
+
+export default Page2;
---
+++ src/compents/Page3.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class Page3 extends Component {
+    render() {
+        return (
+            <div className="Page3">
+
+            </div>
+        );
+    }
+}
+
+export default Page3;
